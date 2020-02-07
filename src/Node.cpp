0bb109a5c02de14dfb0f05ecf0c2ce573258f5fb
jvandeweyer <jelle.vandeweyer@scarlet.be>
2020-02-07 23:50:24 +0100

    Extend bisection method to find all neighbours.

--- src/Node.cpp
+++ src/Node.cpp
@@ -20,16 +20,18 @@ void add_neighbour(std::vector<std::pair<Velocity, Angle>>& neighbours, Velocity
 }
 
 template <std::int32_t... args>
-void visit(Velocity const&                          vf,
-           int                                      vx_min,
-           int                                      vy_min,
-           StaticMatrix<bool, args...>&             visited,
-           std::vector<std::pair<Velocity, Angle>>& neighbours,
-           Velocity const&                          min,
-           Velocity const&                          max,
-           std::int32_t                             yaw_min,
-           std::int32_t                             yaw_max)
+void bisection(
+  Velocity const&                          vf,
+  int                                      vx_min,
+  int                                      vy_min,
+  StaticMatrix<bool, args...>&             visited,
+  std::vector<std::pair<Velocity, Angle>>& neighbours,
+  Velocity const&                          min,
+  Velocity const&                          max,
+  std::int32_t                             yaw_min,
+  std::int32_t                             yaw_max)
 {
+  assert(yaw_min < yaw_max);
   auto const yaw_range = yaw_max - yaw_min;
   assert(yaw_range % 2 == 0);
   auto const yaw = yaw_range / 2 + yaw_min;
@@ -40,8 +42,8 @@ void visit(Velocity const&                          vf,
 
   if (yaw_range == 2)
   {
-    auto& entry = visited.value(static_cast<int>(neighbour[0]) - vx_min, static_cast<int>(neighbour[1]) - vy_min);
-    if (!entry)
+    if (auto& entry = visited.value(static_cast<int>(neighbour[0]) - vx_min, static_cast<int>(neighbour[1]) - vy_min);
+        !entry)
     {
       entry = true;
       add_neighbour(neighbours, neighbour, yaw);
@@ -52,27 +54,27 @@ void visit(Velocity const&                          vf,
 
   if (neighbour == min)
   {
-    visit(vf, vx_min, vy_min, visited, neighbours, min, max, yaw, yaw_max);
+    bisection(vf, vx_min, vy_min, visited, neighbours, min, max, yaw, yaw_max);
   }
   else if (neighbour == max)
   {
-    visit(vf, vx_min, vy_min, visited, neighbours, min, max, yaw_min, yaw);
+    bisection(vf, vx_min, vy_min, visited, neighbours, min, max, yaw_min, yaw);
   }
   else
   {
-    auto& entry = visited.value(static_cast<int>(neighbour[0]) - vx_min, static_cast<int>(neighbour[1]) - vy_min);
-    if (!entry)
+    if (auto& entry = visited.value(static_cast<int>(neighbour[0]) - vx_min, static_cast<int>(neighbour[1]) - vy_min);
+        !entry)
     {
       entry = true;
       add_neighbour(neighbours, neighbour, yaw);
     }
-    visit(vf, vx_min, vy_min, visited, neighbours, min, neighbour, yaw_min, yaw);
-    visit(vf, vx_min, vy_min, visited, neighbours, neighbour, max, yaw, yaw_max);
+    bisection(vf, vx_min, vy_min, visited, neighbours, min, neighbour, yaw_min, yaw);
+    bisection(vf, vx_min, vy_min, visited, neighbours, neighbour, max, yaw, yaw_max);
   }
 }
-}
+} // namespace
 
-std::vector<std::pair<Velocity, Angle>> Node::neighbours2() const
+std::vector<std::pair<Velocity, Angle>> Node::neighbours() const
 {
   std::vector<std::pair<Velocity, Angle>> neighbours;
 
@@ -91,20 +93,54 @@ std::vector<std::pair<Velocity, Angle>> Node::neighbours2() const
   assert(a_int <= 100);
   StaticMatrix<bool, 0, a_int - 1, 0, a_int - 1> visited;
 
+  float const d_vel = std::atan2(vf[1], vf[0]);
+  float const d_opt = std::acos((320 - a) / vf.VectorLength());
+
+  // Use the bisection method to find (almost) all neighbours.
+  // Start searching from the velocity direction such that we don't miss that one either.
+  std::int32_t init = std::round(d_vel * 32768 / M_PI);
+
   Velocity neighbour = vf;
-  Ground::accelerate(neighbour, Angle(0));
+  Ground::accelerate(neighbour, Angle(init));
   neighbour.snap();
 
-  auto& entry = visited.value(static_cast<int>(neighbour[0]) - vx_min, static_cast<int>(neighbour[1]) - vy_min);
-  entry = true;
-  add_neighbour(neighbours, neighbour, 0);
+  {
+    auto& entry = visited.value(static_cast<int>(neighbour[0]) - vx_min, static_cast<int>(neighbour[1]) - vy_min);
+    entry       = true;
+    add_neighbour(neighbours, neighbour, init);
+  }
+  bisection(vf, vx_min, vy_min, visited, neighbours, neighbour, neighbour, init, init + 65536);
 
-  visit(vf, vx_min, vy_min, visited, neighbours, neighbour, neighbour, 0, 65536);
+  // The bisection method can miss the neighbours around the optimal accel angles.
+  // Positive optimal accel angle.
+  std::int32_t const pos_opt = std::ceil((d_vel + d_opt) * 32768 / M_PI);
+  neighbour                  = vf;
+  Ground::accelerate(neighbour, Angle(pos_opt));
+  neighbour.snap();
+
+  if (auto& entry = visited.value(static_cast<int>(neighbour[0]) - vx_min, static_cast<int>(neighbour[1]) - vy_min);
+      !entry)
+  {
+    entry = true;
+    add_neighbour(neighbours, neighbour, pos_opt);
+  }
+  // Negative optimal accel angle.
+  std::int32_t const neg_opt = std::floor((d_vel - d_opt) * 32768 / M_PI);
+  neighbour                  = vf;
+  Ground::accelerate(neighbour, Angle(neg_opt));
+  neighbour.snap();
+
+  if (auto& entry = visited.value(static_cast<int>(neighbour[0]) - vx_min, static_cast<int>(neighbour[1]) - vy_min);
+      !entry)
+  {
+    entry = true;
+    add_neighbour(neighbours, neighbour, neg_opt);
+  }
 
   return neighbours;
 }
 
-std::vector<std::pair<Velocity, Angle>> Node::neighbours() const
+std::vector<std::pair<Velocity, Angle>> Node::neighbours2() const
 {
   std::vector<std::pair<Velocity, Angle>> neighbours;
 
@@ -129,8 +165,8 @@ std::vector<std::pair<Velocity, Angle>> Node::neighbours() const
     Ground::accelerate(neighbour, Angle(yaw));
     neighbour.snap();
 
-    auto& entry = visited.value(static_cast<int>(neighbour[0]) - vx_min, static_cast<int>(neighbour[1]) - vy_min);
-    if (!entry)
+    if (auto& entry = visited.value(static_cast<int>(neighbour[0]) - vx_min, static_cast<int>(neighbour[1]) - vy_min);
+        !entry)
     {
       entry = true;
       add_neighbour(neighbours, neighbour, yaw);
